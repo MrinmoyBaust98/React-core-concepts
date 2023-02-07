4c31484167f6a4b3371343193d9c859896e92d88
MrinmoyBaust98 <baust.cse.170201073@gmail.com>
2023-02-08 03:59:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App grid">
+      <Crickboss name="Sakib Al Hasan" Position="Al-Rounder" Country="Bangladesh"></Crickboss>
+      <Crickboss name="MS Dhoni" Position="Wicket&Batsman" Country="India"></Crickboss>
+      <Crickboss name="Liton Das" Position="Wicket&Batsman" Country="Bangladesh"></Crickboss>
+      <Crickboss name="Dj Bravo" Position="Al-Rounder" Country="Westindies"></Crickboss>
+      <Crickboss name="Mike Hussy" Position="Batsman" Country="Australia"></Crickboss>
+      <Crickboss name="Mark Stonis" Position="Al-Rounder" Country="Australia"></Crickboss>
+      <Crickboss name="Watson" Position="Al-Rounder" Country="Australia"></Crickboss>
+      <Crickboss name="David Warner" Position="Batsman" Country="Australia"></Crickboss>
+      <Crickboss name="AB De villiers" Position="Batsman" Country="South Africa"></Crickboss>
+      <Crickboss name="Brendon McCullum" Position="Batsman" Country="Newzeland"></Crickboss>
+      <Crickboss name="Robi Chandro Aswin" Position="Blower" Country="India"></Crickboss>
+    </div>
+  );
+}
+
+function Crickboss(props){
+   
+  const st={
+    color:"purple",
+    fontSize:"2rem"
+  }
+  return (
+    <div className='boss-design'>
+      <h2><span style={st}>Name: </span>{props.name}</h2>
+      <h4><span style={st}>Position: </span>{props.Position}</h4>
+      <h4><span style={st}>Country: </span>{props.Country}</h4>
     </div>
   );
 }
